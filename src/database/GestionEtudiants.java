4066c9b92eebd33458e956c549ef14d2633cae6c
Abdouldav-cyber <davoudramane89@gmail.com>
2024-10-16 09:10:40 +0200

    mysqlok?!?

--- src/database/GestionEtudiants.java
+++ src/database/GestionEtudiants.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class GestionEtudiants {
-    private static final String URL = "jdbc:mysql://localhost:3306/votre_base";
-    private static final String USER = "votre_utilisateur";
-    private static final String PASSWORD = "votre_mot_de_passe";
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_etudiants";
+    private static final String USER = "root";
+    private static final String PASSWORD = "davou64598258";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
